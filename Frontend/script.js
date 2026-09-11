f0d73ae2defbf29d816fde67f3ffa2abb9f8f860
sisir senapati <sisir.senapati@gmail.com>
2026-09-11 09:53:11 +0530

    PDF comprase

---
+++ Frontend/script.js
@@ -0,0 +1,35 @@
+async function compressPDF() {
+
+    const file =
+        document.getElementById("pdf").files[0];
+
+    if (!file) {
+        alert("Select PDF");
+        return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await fetch(
+        "https://YOUR-RENDER-URL.onrender.com/compress",
+        {
+            method: "POST",
+            body: formData
+        }
+    );
+
+    const blob = await response.blob();
+
+    const url =
+        window.URL.createObjectURL(blob);
+
+    const a =
+        document.createElement("a");
+
+    a.href = url;
+    a.download = "compressed.pdf";
+
+    a.click();
+}
---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import pikepdf
+import tempfile
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+@app.post("/compress")
+async def compress_pdf(file: UploadFile = File(...)):
+
+    input_path = tempfile.mktemp(".pdf")
+    output_path = tempfile.mktemp(".pdf")
+
+    with open(input_path, "wb") as f:
+        f.write(await file.read())
+
+    pdf = pikepdf.Pdf.open(input_path)
+
+    pdf.save(
+        output_path,
+        compress_streams=True,
+        object_stream_mode=pikepdf.ObjectStreamMode.generate
+    )
+
+    pdf.close()
+
+    return FileResponse(
+        output_path,
+        media_type="application/pdf",
+        filename="compressed.pdf"
+    )
